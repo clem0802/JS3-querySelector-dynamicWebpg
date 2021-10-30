225a05fe81d63fd979be76f02e6fcbc577e319c2
clem0802 <88548513+clem0802@users.noreply.github.com>
2021-10-30 19:17:09 +0200

    Create 1.5GettingSettingAttributes.js

---
+++ 1.5GettingSettingAttributes.js
@@ -0,0 +1,161 @@
+// 2021.10.30 FROM (MIMO) 
+/*----------------------------------*/
+// GETTING SETTING ATTRIBUTES
+// use JavaScript METHODS to change HTML attributes, like the links to an image for example
+
+
+/*----------------------------------*/
+/*----------------------------------*/
+//! img ELEMENT stored inside the "el" VARIABLE
+// to set an ATTRIBUTE, use the  setAttribute() METHOD after the VARIABLE name
+
+//?? (HTML)
+<!doctype html>
+<html>
+    <body> 
+        <button onclick="changeImage()">Try it</button>
+        <br>
+        <img src="https://mimo.app/r/cat.png">
+        <script src="script.js"></script>
+    </body>
+</html>
+
+//?? (JavaScript)
+function changeImage() {
+    var el = document.querySelector("img"); //! img ELEMENT stored inside the "el" VARIABLE
+    el.setAttribute();
+}
+
+
+
+
+
+/*----------------------------------*/
+/*----------------------------------*/
+// inside the parentheses, need to specify which ATTRIBUTE we want to set, like "src" in this case
+
+//?? (HTML)
+<!doctype html>
+<html>
+    <body> 
+        <button onclick="changeImage()">Try it</button>
+        <br>
+        <img src="https://mimo.app/r/cat.png">
+        <script src="script.js"></script>
+    </body>
+</html>
+
+//?? (JavaScript)
+function changeImage() {
+    var el = document.querySelector("img"); 
+    el.setAttribute("src"); //! we want to set "SRC"
+}
+
+
+
+
+
+/*----------------------------------*/
+/*----------------------------------*/
+// next, we provide the NEW VALUE for "SRC"
+// we add the NEW VALUE after a COMMA and inside QUOTES
+// press the button to change the LINK
+
+//?? (HTML)
+<!doctype html>
+<html>
+    <body> 
+        <button onclick="changeImage()">Try it</button>
+        <br>
+        <img src="https://mimo.app/r/cat.png">
+        <script src="script.js"></script>
+    </body>
+</html>
+
+//?? (JavaScript)
+function changeImage() {
+    var el = document.querySelector("img"); 
+    el.setAttribute("src","https://mimo.app/r/kittles.png"); //! CHANGE THE LINK
+}
+
+
+
+
+
+/*----------------------------------*/
+/*----------------------------------*/
+//! CHANGE-SET ATTRIBUTE VALUE TO A NEW ONE
+//! display el.getAttribute("src")
+// to see that the WEB ADDRESS actually changed
+
+//?? (HTML)
+<!doctype html>
+<html>
+    <body> 
+        <button onclick="changeImage()">Try it</button>
+        <br>
+        <img src="https://mimo.app/r/cat.png">
+        <script src="script.js"></script>
+    </body>
+</html>
+
+//?? (JavaScript)
+function changeImage() {
+    var el = document.querySelector("img"); 
+    el.setAttribute("src","https://mimo.app/r/kittles.png"); //! (SEE BELOW)
+    console.log(el.getAttribute("src")); //! DISPLAY 
+}
+//! we SPECIFY inside the parentheses of  setAttribute()
+//! BOTH the ATTRIBUTE we want to change and the NEW VALUE
+
+
+
+
+
+/*----------------------------------*/
+/*----------------------------------*/
+//! change the "type" fot eh "input" element to "range"
+
+//?? (HTML)
+<!doctype html>
+<html>
+    <body> 
+        <button onclick="changeType()">Try it</button>
+        <br>
+        <input type="date">
+        <script src="script.js"></script>
+    </body>
+</html>
+
+//?? (JavaScript)
+function changeType() {
+    var el = document.querySelector("input"); 
+    el.setAttribute("type","range"); 
+}
+// (output) first there is a calendar
+// (output) when click on "Try it", the calender changed to a RANGE BAR
+
+
+
+
+
+/*----------------------------------*/
+/*----------------------------------*/
+// add a PLACEHOLER element and set its value to "1234"
+
+//?? (HTML)
+<!doctype html>
+<html>
+    <body> 
+        <button onclick="updateElement()">Add placeholder</button>
+        <br>
+        <input type="password">
+        <script src="script.js"></script>
+    </body>
+</html>
+
+//?? (JavaScript)
+function updateElement() {
+    var el = document.querySelector("input"); 
+    el.setAttribute("placeholder","1234"); 
+}
